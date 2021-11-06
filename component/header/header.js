32f08e6f12609b3abf63097a471d4e08b6324eab
kharismarizki <kharisma.rizki890@gmail.com>
2021-11-06 10:02:20 +0700

    initialize web dan membuat navbar

---
+++ component/header/header.js
@@ -0,0 +1,8 @@
+const header = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+export default header;
---
+++ component/navbar/navbar.js
@@ -0,0 +1,55 @@
+import Styles from "./Navbar.module.css";
+import Link from "next/link";
+const navbar = () => {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-light navbar-expand-lg fixed-top"
+        style={{ background: "#276678" }}
+        id="mainNav"
+      >
+        <div className="container">
+          <a className="navbar-brand text-white" href="/">
+            Rentacar
+          </a>
+          <button
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive"
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fa fa-align-justify" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link href="/">
+                  <a className={Styles.tulisan}>Home</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="#mobil">
+                  <a className={Styles.tulisan}>Daftar Mobil</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="#testimoni">
+                  <a className={Styles.tulisan}>Testimoni</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="#about">
+                  <a className={Styles.tulisan}>Tentang Kami</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default navbar;
