f3171f51b0d1cfe042fdcb5b709b4b0fd7fccacf
kharismarizki <kharisma.rizki890@gmail.com>
2021-11-09 20:18:40 +0700

    menambah card

--- component/daftarmobil/card.js
+++ component/daftarmobil/card.js
@@ -1,19 +1,54 @@
+import styles from "./Card.module.css";
 const card = () => {
   return (
     <>
-      <div className="card" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
+      <div className="card" style={{ width: "20rem" }}>
+        <img
+          src="https://dummyimage.com/300"
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <div className={styles.position}>
+            <h5 className="card-title">
+              <b>Mitsubishi Pajero</b>
+            </h5>
+            <div>Rp.300.0000/hari</div>
+          </div>
+          <div className={styles.transmisi}>Manual</div>
+          <div className="card-body">
+            <center>
+              <div className="row">
+                <div className="col">
+                  <b className="bi bi-people-fill"> Penumpang</b>
+                  <br />
+                  <div>4</div>
+                </div>
+                <div className="col">
+                  <b className="bi bi-bag-check-fill"> Bagasi</b>
+                  <br />
+                  <div>4</div>
+                </div>
+              </div>
+            </center>
+          </div>
+          <center>
+            <div className="row">
+              <div className="col">
+                <button type="button" class="btn btn-primary btn-sm">
+                  Selengkapnya
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" class="btn btn-primary btn-sm">
+                  Pesan Sekarang
+                </button>
+              </div>
+            </div>
+          </center>
         </div>
       </div>
     </>
   );
 };
+export default card;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Navbar from "../component/navbar/navbar";
 import Footer from "../component/footer/footer";
 import CaraPesan from "../component/carapesan/carapesan";
 import DaftarMobil from "../component/daftarmobil/daftarmobil";
+import Card from "../component/daftarmobil/card";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <DaftarMobil />
+      <div className="container">
+        <DaftarMobil />
+        <Card />
+      </div>
       <Footer />
     </div>
   );
