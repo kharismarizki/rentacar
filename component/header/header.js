eb9577465a9c2face026f00310d0bc94a3bd3566
veralindha <30642445+veralindha@users.noreply.github.com>
2021-11-10 20:23:33 +0700

    change header

--- component/header/header.js
+++ component/header/header.js
@@ -2,20 +2,46 @@ const header = () => {
   return (
     <div>
       <header
-        className="text-center text-black  d-flex masthead"
-        style={{ backgroundImage: "url(/header.png)" }}
+        className="text-center text-white d-flex"
+        id="about"
+        style={{ backgroundImage: 'url("/header.png")' }}
       >
         <div className="container my-auto">
           <div className="row">
             <div className="col-lg-10 mx-auto">
-              <h1>tes</h1>
+              <h1 className="text text-dark text-uppercase">
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <strong>RENTACAR</strong>
+              </h1>
+              <hr />
             </div>
           </div>
           <div className="col-lg-8 mx-auto">
-            <p>tes</p>
+            <h2 className="text text-dark text-faded mb-5">
+            Sewa sebuah Mobil Hanya dengan menggunakan jari
+            </h2>
+            <button
+              className="btn btn-primary btn-xl"
+              role="button"
+              href="#transaksi"
+              style={{ background: "#1687a7"}}
+            >
+              RENTAL SEKARANG
+            </button>
           </div>
+          <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
         </div>
+        
       </header>
+      <br/>
     </div>
   );
 };
