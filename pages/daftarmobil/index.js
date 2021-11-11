7c4cc184c05899daee2be5c1842386478fc71a73
veralindha <30642445+veralindha@users.noreply.github.com>
2021-11-11 12:34:53 +0700

    Add files via upload

---
+++ pages/daftarmobil/index.js
@@ -0,0 +1,39 @@
+import Card from "../../component/card/card";
+const daftarMobil = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>DAFTAR MOBIL</h1>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col">
+            <Card />
+          </div>
+          <div className="col">
+            <Card />
+          </div>
+          <div className="col">
+            <Card />
+          </div>
+        </div>
+        <br/>
+        <div className="row">
+          <div className="col">
+            <Card />
+          </div>
+          <div className="col">
+            <Card />
+          </div>
+          <div className="col">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default daftarMobil;
